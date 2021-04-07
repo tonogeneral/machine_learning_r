d9dba18c1f256efe5b9eb721d90db35b4d61ea72
Gabriel General <tonogeneral@gmail.com>
2021-04-07 17:42:55 -0400

    Se agregan comentarios descriptivos

--- chapter_1.R
+++ chapter_1.R
@@ -1,4 +1,8 @@
+#Código de curso Machine learning en R
+
+
 heart_dataframe<-read.csv(url("http://dataaspirant.com/wp-content/uploads/2017/01/heart_tidy.csv"))
 
 str(heart_dataframe)
 head(heart_dataframe)
+
